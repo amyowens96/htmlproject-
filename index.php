d9582e5bf9e40143cf414d8791251691536bc123
unknown <amyowens96@gmail.com>
2018-11-27 19:29:27 +0000

    html project

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+	
+		<title>Index</title>
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	
+		<!-- template style sheet -->
+		<link rel="stylesheet" href="template.css">
+
+	</head>
+
+	<body>
+	
+		<!-- nav bar -->
+			<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+				<a class="navbar-brand" href="#"><img id= "workvivo_logo" src ="logo.png"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+				
+				<li class="nav-item active">
+					<a class="nav-link" href="./knowledgebase/HomePage.html">Knowledgebase</a>
+				</li>
+
+				<li class="nav-item dropdown active">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Ticketing
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="ticketingsystem/TicketHomePage.html">Ticket Home Page</a>
+							<!-- <a class="dropdown-item" href="ticketingsystem/DetailsTicketPage.html">Details</a> -->
+					<!-- <div class="dropdown-divider"></div> -->
+							<a class="dropdown-item" href="ticketingsystem/CreateTicketPage.html">Create Ticket</a>
+					</div>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="FAQ/FaqPage.html">FAQ</a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="knowledgebase/AboutUsPage.html">About Us</a>
+				</li>
+				</ul>
+			<form class="form-inline my-2 my-lg-0">
+			  <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+			  <a href= "https://www.workvivo.com/demo"><button class="btn btn-warning my-2 my-sm-0" type="button">Request Demo</button></a>
+			  
+			</form>
+		  </div>
+		</nav>
+		
+		<!-- break line -->
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		
+		<!-- new div -->
+		<div class="container-fluid">
+		
+		
+		<!-- Deck of cards -->
+		<div class="card-deck">	
+				<div class="card">
+				<a href= "knowledgebase/HomePage.html" ><img class="card-img-top"  src="knowledgebase.png" alt="Card image cap">
+				<div class="card-body">
+				  <h5 class="card-title">Knowledgebase</h5></a>
+				  <p class="card-text">Find Articles on this page.</p>
+				  <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+				</div>
+			  </div>
+			  <div class="card">
+				<a href= "ticketingsystem/TicketHomePage.html" ><img class="card-img-top"  src="ticketingsystemimage.png" alt="Card image cap">
+				<div class="card-body">
+				  <h5 class="card-title">Ticketing System</h5></a>
+				  <p class="card-text">Create a ticket and view previously made tickets in this page.</p>
+				  <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+				</div>
+			  </div>
+			  <div class="card">
+				<a href = "faq/FaqPage.html"><img class="card-img-top" src="faqimage.png" alt="Card image cap">
+				<div class="card-body">
+				  <h5 class="card-title">FAQ</h5></a>
+				  <p class="card-text">You can find the answers to all the frequently asked questions here.</p>
+				<!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+				</div>
+			  </div>
+			  <div class="card">
+				<a href = "knowledgebase/AboutUsPage.html"><img class="card-img-top" src="aboutusimage.png" alt="Card image cap">
+				<div class="card-body">
+				  <h5 class="card-title">About us</h5></a>
+				  <p class="card-text">Our Story. Find out who we are, what do we do and where we started.</p>
+				  <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+				</div>
+			  </div>
+		</div> 
+		
+		</div>
+		
+		<br>
+		<br>
+		<br>
+		
+		<!-- footer -->
+		
+		<footer>
+		  <div class="container">
+			<span class="text-muted">
+			<center> <font color="white"> Some of the world's leading organisations are using Workvivo. </font> </center>
+			</span>
+			
+			<br>
+			
+			<!-- images from workvivo page -->
+			
+			<center>
+			<a href = "https://www.morganmckinley.ie/"><img src="https://www.workvivo.com/img/customers/mmk.png" height="175" width="175" hspace="60" class="img-fluid"></a>
+			<a href = "http://www.staffs.ac.uk/"><img src="https://www.workvivo.com/img/customers/staffs-logo-2.png" height="115" width="115" hspace="60" class="img-fluid"></a>
+			<a href = "https://www.voxprogroup.com/"><img src="https://www.workvivo.com/img/customers/voxpro.png" height="150" width="150" hspace="60" class="img-fluid"></a>
+			<a href = "https://www.trigonhotels.com/"><img src="https://www.workvivo.com/img/customers/trigon.png" height="125" width="125" hspace="60" class="img-fluid"></a>
+			</center>
+			<br>
+		
+		
+		 
+		    
+		  
+		</footer>
+		
+		<a href="#top"><img class="footer-logo mr-4" src="https://www.workvivo.com/img/logo-icon.png"height="50" width="50"></a>
+           <a href="#top" class="text-muted mr-4"><font color="white">Back to Top </font></a>
+		  
+		  
+		  <!-- <div class="text-right"> -->
+                <a href="https://twitter.com/workvivo" target="_blank" class="footer-social-link mr-sm-4"><img src="https://www.workvivo.com/img/icons/twitter.svg"height="50" width="50"></a>
+				<a href="https://www.linkedin.com/company/workvivo/" target="_blank" class="footer-social-link"><img src="https://www.workvivo.com/img/icons/linkedin.svg"height="50" width="50"></a>
+            <!-- </div> -->
+			
+		<p> <center> <font color="white"> &copy 2018 Workvivo Limited. All rights reserved. </font></center> </p>
+		<br>
+		<br>
+		<br>
+		
+       
+		
+		 <!-- <script src="/js/app.js?id=f8c30a6864752dd01954"></script> -->
+				<!-- <script> -->
+				<!-- window.intercomSettings = { -->
+					<!-- app_id: "t9ofwu9f" -->
+				<!-- }; -->
+			<!-- </script> -->
+			<!-- <script> -->
+				<!-- (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/t9ofwu9f';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})() -->
+			<!-- </script> -->
+		<!-- <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4674675.js"></script> -->
+		 
+		
+		
+	</body>
+</html>
